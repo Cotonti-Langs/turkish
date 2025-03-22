03491df23079cc057c91f0582023e93830857156
Ali Çömez | Slaweally <sys@rootali.net>
2025-03-22 17:56:34 +0300

    Create i18n.tr.lang.php

---
+++ plugins/i18n/lang/i18n.tr.lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * İçerik Uluslararasılaştırma Eklentisi için Türkçe Dil Dosyası
+ *
+ * @package I18n
+ * @copyright (c) Cotonti Takımı
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ */
+
+defined('COT_CODE') or die('Yanlış URL.');
+
+// Eklenti Ayarları
+
+$L['cfg_cats'] = 'Uluslararasılaştırma uygulanacak ana kategoriler';
+$L['cfg_cats_hint'] = 'Virgülle ayrılmış kategori kodları';
+$L['cfg_locales'] = 'Site dillerinin listesi';
+$L['cfg_locales_hint'] = 'Her satıra bir dil, format: dil_kodu|Dil Başlığı';
+$L['cfg_omitmain'] = 'Ana dile yönlendiren URL’lerde dil parametresini gizle';
+$L['cfg_rewrite'] = 'Dil parametresi için URL yeniden yazmayı etkinleştir';
+$L['cfg_rewrite_hint'] = 'Manuel .htaccess güncellemesi gerektirir';
+$L['cfg_cookie'] = 'Dil tercihini çerezle hatırla';
+
+$L['info_desc'] = 'Sayfalar, kategoriler, etiketler gibi içeriklerin çok dilli olmasını sağlayan yerelleştirme aracı';
+
+// Eklenti Metinleri
+
+$L['i18n_adding'] = 'Yeni çeviri ekle';
+$L['i18n_confirm_delete'] = 'Çeviriyi silmek istediğinizden emin misiniz?';
+$L['i18n_delete'] = 'Çeviriyi sil';
+$L['i18n_editing'] = 'Çeviri düzenle';
+$L['i18n_incorrect_locale'] = 'Geçersiz dil ayarı';
+$L['i18n_items_added'] = '{$cnt} öğe eklendi';
+$L['i18n_items_removed'] = '{$cnt} öğe kaldırıldı';
+$L['i18n_items_updated'] = '{$cnt} öğe güncellendi';
+$L['i18n_locale_selection'] = 'Dil Seçimi';
+$L['i18n_localized'] = 'Yerelleştirilmiş';
+$L['i18n_no_categories'] = 'Çeviri için seçili kategori yok. Kategorileri <a href="%s">Uluslararasılaştırma ayarları</a> sayfasından ayarlayabilirsiniz.';
+$L['i18n_original'] = 'Orijinal';
+$L['i18n_structure'] = 'Yapı Uluslararasılaştırması';
+$L['i18n_translate'] = 'Çevir';
+$L['i18n_translation'] = 'Çeviri';
